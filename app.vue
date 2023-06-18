e6ebc6f03eff14b95e2288d0114cb56130080a79
Estee <estee.tsw@gmail.com>
2023-06-18 16:18:20 +0800

    feat: add support for more languages
    - Add support for Korean language by importing ko.json in plugins/i18n.ts
    - Add support for Spanish language by importing es.json in plugins/i18n.ts
    
    feat: optimize lang dropdown to show options based on available locales
    
    - Modify app.vue to show lang dropdown options based on available locales
    - Locales are generated using deepl-translate-github-action
    - Update README.md to reflect the changes made to the project

--- app.vue
+++ app.vue
@@ -1,14 +1,19 @@
 <template>
-  <div>
-    <h1>{{ $t('hello', { name: 'vue-i18n' }) }}</h1>
-    <p>{{ $t('description') }}</p>
-+    <form>
-+      <label for="locale-select">{{ $t('language') }}: </label>
-+      <select id="locale-select" v-model="$i18n.locale">
-+        <option value="en">en</option>
-+        <option value="fr">fr</option>
-+        <option value="ja">ja</option>
-+      </select>
-+    </form>
-  </div>
+	<div>
+		<h1>{{ $t("hello", { name: "vue-i18n" }) }}</h1>
+		<p>{{ $t("description") }}</p>
+		<form>
+			<label for="locale-select">{{ $t("language") }}: </label>
+			<select
+				id="locale-select"
+				v-model="$i18n.locale">
+				<option
+					v-for="(locale, index) in $i18n.availableLocales"
+					:key="index"
+					:value="locale">
+					{{ locale }}
+				</option>
+			</select>
+		</form>
+	</div>
 </template>
--- plugins/i18n.ts
+++ plugins/i18n.ts
@@ -2,6 +2,8 @@ import { createI18n } from 'vue-i18n'
 import en from '../locales/en.json'
 import fr from '../locales/fr.json'
 import ja from '../locales/ja.json'
+import ko from '../locales/ko.json'
+import es from '../locales/es.json'
 
 export default defineNuxtPlugin(({ vueApp }) => {
   const i18n = createI18n({
@@ -11,7 +13,9 @@ export default defineNuxtPlugin(({ vueApp }) => {
     messages: {
       en,
       fr,
-      ja
+      ja,
+      ko,
+      es
     }
   })
 
